bde869c7e9bb25359b4513a1209a34f6d5a90b6b
Mirko <mirkosbaglia2001@gmail.com>
2021-03-26 14:15:47 +0100

    Esercizio Opzionale sul file script2.js

---
+++ js/script2.js
@@ -0,0 +1,31 @@
+//OPZIONE
+// Scrivi una funzione che accetti tre argomenti:
+// un array e due numeri ("a" più piccolo di "b" e "b" grande al
+// massimo quanto il numero di elementi dell'array).
+// La funzione ritornerà un nuovo array con i valori che
+// hanno la posizione compresa tra "a" e "b"
+function splitter(min, max, array) {
+  var arrayFinale = [];
+  // console.log(min , max, array);
+  for (var i = 0; i < array.length; i++) {
+     // console.log(array[i]);
+     var singleNum = array[i];
+     if (singleNum > 3 && singleNum <= 7) {
+       arrayFinale.push(singleNum)
+     }
+
+  }
+
+ console.log(arrayFinale);
+
+}
+function esArraySplitter() {
+    var arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+    var indMin = 3;
+    var indMax = 7;
+    var splitted = splitter(indMin, indMax, arr);
+
+    // [ 4, 5, 6, 7 ]
+}
+
+$(document).ready(esArraySplitter);
